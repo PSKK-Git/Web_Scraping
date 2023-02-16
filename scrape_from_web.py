2beb088dd790a1de32e56da20cab13d4158f1e5e
karthik <saikarthik8106@gmail.com>
2023-02-16 19:12:23 +0530

    made pretty

--- scrape_from_web.py
+++ scrape_from_web.py
@@ -1,26 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-html_text=requests.get('https://internshala.com/internships/work-from-home-web-development-internships/').text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs = soup.find('div', class_ ='container-fluid individual_internship visibilityTrackerItem')
+def find_jobs():
+    html_text=requests.get('https://internshala.com/internships/work-from-home-web-development-internships/').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('div', class_ ='container-fluid individual_internship visibilityTrackerItem')
+    for job in jobs:
+        stipend= job.find('span', class_='stipend').text
+        if '10,000' in stipend:
 
-role =  jobs.find('h3', class_='heading_4_5 profile').text
-company_name = jobs.find('a', class_='link_display_like_text view_detail_button').text.replace(' ','')
-starting= jobs.find('div', class_ ='item_body').text.replace(' ','')
-stipend= jobs.find('span', class_='stipend').text
-status= jobs.find('div', class_='other_label_container').text
-#print(company_name)
-#print(jobs)
-print(f'''
-Role : {role}
+            role =  job.find('h3', class_='heading_4_5 profile').text
+            company_name = job.find('a', class_='link_display_like_text view_detail_button').text.replace(' ','')
+            starting= job.find('div', class_ ='item_body').text.replace(' ','')
+            status= job.find('div', class_='other_label_container').text
+            #more_info=job.div.h3.a['herf ']
+            # print(f'''
+            #  Role : {role}
+            #
+            #  Company Name: {company_name}
+            #
+            #  Starting From: {starting}
+            #
+            #  Stipend:  {stipend}
+            #
+            #  Status:  {status}
+            #
+            #  NEXT
+            #  ''')
+            print(f"Role : {role.strip()}")
+            print(f"Company Name: {company_name.strip()}")
+            print(f"Stipend:  {stipend.strip()}")
+           # print(f'More Info: {more_info}')
+            print('')
 
-Company Name: {company_name}
-
-Starting From: {starting}
-
-Stipend: {stipend}
-
-Status: {status}
-''')
-
+if __name__ == '__scrape_from_web__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes...')
+        time.sleep(time_wait*60)
